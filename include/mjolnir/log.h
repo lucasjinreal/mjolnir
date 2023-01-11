195afc790e7b8685b4c8a68ca581fd7fcd4ae996
lewisjin <you@example.com>
2023-01-11 12:49:01 +0800

    fix sys/time.h on windows

--- include/mjolnir/log.h
+++ include/mjolnir/log.h
@@ -8,7 +8,49 @@
 #include <sstream>
 #include <string>
 #include <sys/types.h>
+#ifndef _MSC_VER
 #include <sys/time.h>
+#endif
+
+#ifdef _WIN32
+#include <sys/timeb.h>
+#include <sys/types.h>
+#include <winsock2.h>
+
+inline int gettimeofday(struct timeval *t, void *timezone) {
+  struct _timeb timebuffer;
+  _ftime64_s(&timebuffer);
+  t->tv_sec = timebuffer.time;
+  t->tv_usec = 1000 * timebuffer.millitm;
+  return 0;
+};
+
+#define __need_clock_t
+#include <time.h>
+
+/* Structure describing CPU time used by a process and its children.  */
+struct tms {
+  clock_t tms_utime; /* User CPU time.  */
+  clock_t tms_stime; /* System CPU time.  */
+
+  clock_t tms_cutime; /* User CPU time of dead children.  */
+  clock_t tms_cstime; /* System CPU time of dead children.  */
+};
+
+/* Store the CPU time used by this process and all its
+   dead children (and their dead children) in BUFFER.
+   Return the elapsed real time, or (clock_t) -1 for errors.
+   All times are in CLK_TCKths of a second.  */
+inline clock_t times(struct tms *__buffer) {
+  __buffer->tms_utime = clock();
+  __buffer->tms_stime = 0;
+  __buffer->tms_cstime = 0;
+  __buffer->tms_cutime = 0;
+  return __buffer->tms_utime;
+}
+
+typedef long long suseconds_t;
+#endif
 
 #define LOG_INFOF(...)                                                         \
   do {                                                                         \
